988b2c2628e7387f75dc60e0ec16446ee1b225e5
Garrick Larson Potts <ricki.glp@gmail.com>
2024-06-27 17:56:06 +0200

    created the api for recipe app

---
+++ backend/src/recipe_api.ts
@@ -0,0 +1,25 @@
+const apiKey = process.env.API_KEY;
+
+const recipeSearch = async(searchTerm: string, page: number) => {
+    if(!apiKey) {
+        throw new Error('The API key could not be found');
+    }
+
+    const url = new URL('https://api.spoonacular.com/recipes/complexSearch');
+
+    const queryParams = {
+        apiKey,
+        query: searchTerm,
+        number: '10',
+        offset: (page * 10).toString()
+    }
+    url.search = new URLSearchParams(queryParams).toString();
+    
+    try {
+        const searchRes = await fetch(url);
+        const resJson = await searchRes.json();
+        return resJson;
+    } catch (error) {
+        console.log(error);
+    }
+}
