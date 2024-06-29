1d73961ff4e6b0c2de09af464178f0736e70d689
Garrick Larson Potts <ricki.glp@gmail.com>
2024-06-30 01:33:30 +0200

    types added api connecting to backend

--- recipe-app/src/App.tsx
+++ recipe-app/src/App.tsx
@@ -1,12 +1,37 @@
-import React from "react";
-import { ReactDOM } from "react";
+import "./App.css";
+import { FormEvent, useState } from "react";
+import * as api from "./api";
+import { Recipe } from "./types";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("burgers");
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  const handleSearchSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      const recipes = await api.searchRecipes(searchTerm, 1);
+      setRecipes(recipes.results);
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
   return (
     <>
-      <div>Hello app here!</div>
+      <div>
+        <form onSubmit={ (event) => handleSearchSubmit(event)}>
+          <button type="submit">Submit</button>
+        </form>
+        {recipes.map((recipe) => (
+          <div>
+            recipe image: {recipe.image}
+            recipe title: {recipe.title}
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 };
 
 export default App;
---
+++ recipe-app/src/api.ts
@@ -0,0 +1,11 @@
+export const searchRecipes = async (searchTerm: string, page: number) => {
+    const buildUrl = new URL("https://localhost:5000/api/recipes/search");
+    buildUrl.searchParams.append('searchTerm', searchTerm);
+    buildUrl.searchParams.append('page', String(page));
+
+    const responce = await fetch(buildUrl);
+    if (!responce.ok) {
+        throw new Error(`HTTP error code: ${responce.status}`);
+    }
+    return responce.json();
+}
